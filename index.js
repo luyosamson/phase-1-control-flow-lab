33490a5ca7a413bb43c99a8b6df943bb87fcb871
luyosamson <luyosamson@gmail.com>
2022-08-16 21:21:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result
+  if(distance<=400){
+    return "This one is on me!";
+  }
+  else if(distance>2000 && distance<2500){
+    return 'I will gladly take your thirty bucks.'
+
+  }
+  else{
+    result ='No can do.';
+  }
+  return result;
+}
+function ternaryCheckCity(city) {
+  // Write your code here!
+
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
